921f8a841b7114977db9fd842219d415463410e9
KaiqueGSilva <kaka.gon.silva@gmail.com>
2022-07-22 16:46:04 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express  from "express"
+import mainRoutes from './routes/index'
+import mustache from 'mustache-express'
+import path from 'path'
+
+
+const server = express()
+
+server.set('view engine','mustache')
+server.set('views',path.join(__dirname,'views'))
+
+server.engine('mustache',mustache())
+
+//HABILITANDO O MÉTODO POST
+
+server.use(express.urlencoded({extended:true}))
+
+server.use(mainRoutes)
+
+server.listen(3000)
